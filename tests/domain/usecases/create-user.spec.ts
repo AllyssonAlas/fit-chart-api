86d009c19f4af3ada8b6bb9a62f0e0496e8c458c
Allysson Alas <allyssonmgs@gmail.com>
2024-02-24 15:35:15 -0300

    refactor: isolate sut

--- tests/domain/usecases/create-user.spec.ts
+++ tests/domain/usecases/create-user.spec.ts
@@ -1,28 +1,35 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
-import { setupCreateUser } from '@/domain/usecases';
+import { CreateUser, setupCreateUser } from '@/domain/usecases';
 import { LoadUserRepository } from '@/domain/contracts/repositories';
 
 describe('CreateUser', () => {
-  it('Should call LoadUserRepository with correct input', async () => {
-    const loadUserRepository = mock<LoadUserRepository>();
-    const sut = setupCreateUser(loadUserRepository);
+  const user = {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    role: 'any_role_id',
+    contact: 'any_contact',
+    address: {
+      city: 'any_city',
+      neighborhood: 'any_neighborhood',
+      number: 'any_number',
+      postalCode: 'any_postal_code',
+      state: 'any_state',
+      street: 'any_street',
+    },
+  };
 
-    await sut({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      role: 'any_role_id',
-      contact: 'any_contact',
-      address: {
-        city: 'any_city',
-        neighborhood: 'any_neighborhood',
-        number: 'any_number',
-        postalCode: 'any_postal_code',
-        state: 'any_state',
-        street: 'any_street',
-      },
-    });
+  let loadUserRepository: MockProxy<LoadUserRepository>;
+  let sut: MockProxy<CreateUser>;
+
+  beforeEach(() => {
+    loadUserRepository = mock();
+    sut = setupCreateUser(loadUserRepository);
+  });
+
+  it('Should call LoadUserRepository with correct input', async () => {
+    await sut(user);
 
     expect(loadUserRepository.load).toHaveBeenCalledWith({ email: 'any_email@mail.com' });
     expect(loadUserRepository.load).toHaveBeenCalledTimes(1);
