6999289a45ee6f7a6e0151ba3c37bd6df6050b82
Allysson Alas <allyssonmgs@gmail.com>
2024-02-25 18:22:32 -0300

    feat: ensure entity returns correct data

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,42 @@
+type Data = {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  contact: string;
+  address: {
+    number: string
+    street: string
+    neighborhood: string
+    city: string
+    state: string
+    postalCode: string
+    complement?: string
+  };
+}
+
+export class User {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  contact: string;
+  address: {
+    number: string
+    street: string
+    neighborhood: string
+    city: string
+    state: string
+    postalCode: string
+    complement?: string
+  };
+
+  constructor(data: Data) {
+    this.name = data.name;
+    this.email = data.email;
+    this.password = data.password;
+    this.role = data.role;
+    this.contact = data.contact;
+    this.address = data.address;
+  }
+}
