ee991baf1057a80c355915666e4786a201bd9066
Allysson Alas <allyssonmgs@gmail.com>
2024-02-25 18:24:32 -0300

    test: ensure entity returns correct data with no required fields

--- tests/domain/entities/user.spec.ts
+++ tests/domain/entities/user.spec.ts
@@ -1,24 +1,32 @@
 import { User } from '@/domain/entities';
 
 describe('User', () => {
-  it('Should return an user', () => {
-    const userData = {
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      role: 'any_role_name',
-      contact: 'any_contact',
-      address: {
-        city: 'any_city',
-        neighborhood: 'any_neighborhood',
-        number: 'any_number',
-        postalCode: 'any_postal_code',
-        state: 'any_state',
-        street: 'any_street',
-      },
-    };
+  const userData = {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    role: 'any_role_name',
+    contact: 'any_contact',
+    address: {
+      city: 'any_city',
+      neighborhood: 'any_neighborhood',
+      number: 'any_number',
+      postalCode: 'any_postal_code',
+      state: 'any_state',
+      street: 'any_street',
+    },
+  };
 
+  it('Should return an user', () => {
     const sut = new User(userData);
+
+    expect(sut).toEqual(userData);
+  });
+
+  it('Should return an user with address complement', () => {
+    const addressWithComplement = { ...userData.address, complement: 'any_complement' };
+
+    const sut = new User({ ...userData, address: addressWithComplement });
 
     expect(sut).toEqual({
       name: 'any_name',
@@ -33,6 +41,7 @@ describe('User', () => {
         postalCode: 'any_postal_code',
         state: 'any_state',
         street: 'any_street',
+        complement: 'any_complement',
       },
     });
   });
