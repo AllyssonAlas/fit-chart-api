dce3c44e5d537e3711950af5c5974e5aa4669236
Allysson Alas <allyssonmgs@gmail.com>
2024-02-29 20:48:29 -0300

    test: ensure CreateUserController returns 400 if address subfield postalCode is not provided

--- tests/application/controllers/create-user.spec.ts
+++ tests/application/controllers/create-user.spec.ts
@@ -143,4 +143,15 @@ describe('CreateUserController', () => {
       body: new Error('Subfield state of field address is required'),
     });
   });
+
+  it('Should return 400 if address subfield postalCode is not provided', async () => {
+    const { address: { postalCode, ...addressWithouField }, ...requestWithoutField } = request;
+
+    const response = await sut.perform({ ...requestWithoutField, address: addressWithouField } as any);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new Error('Subfield postalCode of field address is required'),
+    });
+  });
 });
