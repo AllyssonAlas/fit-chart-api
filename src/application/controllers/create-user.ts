2b365b6f4d1860a3dbfb9a976b980bbb078d83ca
Allysson Alas <allyssonmgs@gmail.com>
2024-02-28 21:32:55 -0300

    feat: ensure CreateUserController returns 400 if field contact is not provided

--- src/application/controllers/create-user.ts
+++ src/application/controllers/create-user.ts
@@ -17,7 +17,7 @@ type Request = {
 
 export class CreateUserController {
   async perform(request: Request): Promise<any> {
-    const requiredFields = ['name', 'email', 'password', 'role'];
+    const requiredFields = ['name', 'email', 'password', 'role', 'contact'];
 
     for (const field of requiredFields) {
       if (!Object.keys(request).includes(field)) {
