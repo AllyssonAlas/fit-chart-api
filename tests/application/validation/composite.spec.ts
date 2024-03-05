4f80a505a01dd0844053352dd2b66a9c7ee7775f
Allysson Alas <allyssonmgs@gmail.com>
2024-03-05 20:05:41 -0300

    refactor: isolate sut creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,23 +1,34 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 import { ValidatorComposite, Validator } from '@/application/validation';
 
 describe('ValidatorComposite', () => {
-  it('Should return undefined if all Validators return undefined', () => {
-    const sut = new ValidatorComposite([]);
+  let sut: ValidatorComposite;
+  let validator1: MockProxy<Validator>;
+  let validator2: MockProxy<Validator>;
+  let validators: Validator[];
 
+  beforeAll(() => {
+    validator1 = mock();
+    validator1.validate.mockReturnValue(undefined);
+    validator2 = mock();
+    validator2.validate.mockReturnValue(undefined);
+    validators = [validator1, validator2];
+  });
+
+  beforeEach(() => {
+    sut = new ValidatorComposite(validators);
+  });
+
+  it('Should return undefined if all Validators return undefined', () => {
     const error = sut.validate();
 
     expect(error).toBeUndefined();
   });
 
   it('Should return first error', () => {
-    const validator1 = mock<Validator>();
     validator1.validate.mockReturnValueOnce(new Error('validator_1_error'));
-    const validator2 = mock<Validator>();
     validator2.validate.mockReturnValueOnce(new Error('validator_2_error'));
-    const validators = [validator1, validator2];
-    const sut = new ValidatorComposite(validators);
 
     const error = sut.validate();
 
