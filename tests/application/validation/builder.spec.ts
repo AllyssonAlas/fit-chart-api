c762417c4736fe775d21e4b187a3f7b11fe671f1
Allysson Alas <allyssonmgs@gmail.com>
2024-03-10 15:48:13 -0300

    test: ensure ValidationBuilder string returns RequiredString validator

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,4 +1,4 @@
-import { Required, RequiredParam, ValidationBuilder } from '@/application/validation';
+import { Required, RequiredParam, RequiredString, ValidationBuilder } from '@/application/validation';
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredParam and Required validators', () => {
@@ -11,6 +11,18 @@ describe('ValidationBuilder', () => {
     expect(validators).toEqual([
       new RequiredParam(data, 'value'),
       new Required(data.value, 'value'),
+    ]);
+  });
+
+  it('Should return RequiredString validator', () => {
+    const data = { value: 'any_value' };
+    const validators = ValidationBuilder
+      .of({ value: data, fieldName: 'value' })
+      .string()
+      .build();
+
+    expect(validators).toEqual([
+      new RequiredString(data.value, 'value'),
     ]);
   });
 });
