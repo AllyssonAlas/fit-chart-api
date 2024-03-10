2dbeb408a8b88449816c50c64a30f2ac3de25d68
Allysson Alas <allyssonmgs@gmail.com>
2024-03-10 15:47:04 -0300

    feat: ensure ValidationBuilder required returns RequiredParam and Required validators

---
+++ src/application/validation/builder.ts
@@ -0,0 +1,23 @@
+import { Required, RequiredParam, Validator } from '@/application/validation';
+
+export class ValidationBuilder {
+  private constructor(
+    private readonly value: any,
+    private readonly fieldName: string,
+    private readonly validators: Validator[] = [],
+  ) {}
+
+  static of({ value, fieldName }: { value: any, fieldName: string }): ValidationBuilder {
+    return new ValidationBuilder(value, fieldName);
+  }
+
+  required(): ValidationBuilder {
+    this.validators.push(new RequiredParam(this.value, this.fieldName));
+    this.validators.push(new Required(this.value[this.fieldName], this.fieldName));
+    return this;
+  }
+
+  build(): Validator[] {
+    return this.validators;
+  }
+}
