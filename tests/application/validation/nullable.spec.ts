624c2f11a72db6d5fc4ac6c02a826722e5d20f8f
Allysson Alas <allyssonmgs@gmail.com>
2025-01-20 12:49:21 -0300

    test: ensure NullableParam returns InvalidParamError if value type is not allowed

--- tests/application/validation/nullable.spec.ts
+++ tests/application/validation/nullable.spec.ts
@@ -2,8 +2,24 @@ import { InvalidParamError } from '@/application/errors';
 import { NullableParam } from '@/application/validation';
 
 describe('NullableParam', () => {
-  it('Should return a InvalidParamError if value type is not allowed', () => {
+  it('Should return an InvalidParamError if value type is not allowed', () => {
     const sut = new NullableParam({}, 'field', 'string');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidParamError('field'));
+  });
+
+  it('Should return an InvalidParamError if value type is not allowed', () => {
+    const sut = new NullableParam({ field: undefined }, 'field', 'string');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidParamError('field'));
+  });
+
+  it('Should return an InvalidParamError if value type is not allowed', () => {
+    const sut = new NullableParam({ field: 123 }, 'field', 'string');
 
     const error = sut.validate();
 
