b68f2b629622fa41979d9f83715e7f591eb3f5c1
Allysson Alas <allyssonmgs@gmail.com>
2025-01-20 17:38:19 -0300

    feat: ensure LoadRoleRepository returns a Role if role name exists

--- src/infra/database/postgres/repositories/role.ts
+++ src/infra/database/postgres/repositories/role.ts
@@ -3,7 +3,9 @@ import { PrismaClient } from '@prisma/client';
 import type { LoadRoleRepository } from '@/domain/contracts/repositories';
 
 export class RoleRepository implements LoadRoleRepository {
-  async load(input: LoadRoleRepository.Input): Promise<LoadRoleRepository.Output> {
-    return null;
+  async load({ name }: LoadRoleRepository.Input): Promise<LoadRoleRepository.Output> {
+    const prisma = new PrismaClient();
+    const role = await prisma.role.findUnique({ where: { name } });
+    return role;
   }
 }
