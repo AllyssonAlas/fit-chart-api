06c9319a31a6fb79fbbdedc0398cdd1bad901da5
Allysson Alas <allyssonmgs@gmail.com>
2025-01-22 19:59:05 -0300

    feat: ensure SaveUserRepository saves User data in database

--- src/infra/database/postgres/repositories/user.ts
+++ src/infra/database/postgres/repositories/user.ts
@@ -1,11 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 
-import type { LoadUserRepository } from '@/domain/contracts/repositories';
+import type { LoadUserRepository, SaveUserRepository } from '@/domain/contracts/repositories';
 
 export class UserRepository implements LoadUserRepository {
   async load(input: LoadUserRepository.Input): Promise<LoadUserRepository.Output> {
     const prisma = new PrismaClient();
     const user = await prisma.user.findUnique({ where: input });
     return user;
+  }
+
+  async save(input: SaveUserRepository.Input): Promise<SaveUserRepository.Output> {
+    const prisma = new PrismaClient();
+    await prisma.user.create({
+      data: {
+        ...input,
+        address: {
+          create: input.address,
+        },
+      },
+    });
   }
 }
