beddf3d098287677e680108d00abccfdbc65af69
Allysson Alas <allyssonmgs@gmail.com>
2025-01-24 18:42:27 -0300

    feat: ensure adaptExpressRoute responds with correct statusCode and error

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,6 +5,7 @@ import type { Controller } from '@/application/controllers';
 export const adaptExpressRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const { data, statusCode } = await controller.handle(req.body);
-    res.status(statusCode).json(data);
+    const json = [200, 204].includes(statusCode) ? data : { error: data.message };
+    res.status(statusCode).json(json);
   };
 };
