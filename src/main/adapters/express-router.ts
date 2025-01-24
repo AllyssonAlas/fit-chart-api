168223245580065097589d59e8110c3ff77055be
Allysson Alas <allyssonmgs@gmail.com>
2025-01-23 23:34:43 -0300

    feat: ensure adaptExpressRoute responds with 200 and correct data

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,6 +4,7 @@ import type { Controller } from '@/application/controllers';
 
 export const adaptExpressRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    controller.handle(req.body);
+    const { data } = await controller.handle(req.body);
+    res.status(200).json(data);
   };
 };
