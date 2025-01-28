cbe7fd63b1678b0992c9ca717cc898feec5cae76
Allysson Alas <allyssonmgs@gmail.com>
2025-01-28 19:36:57 -0300

    feat: create route POST for /user/create

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,7 +1,8 @@
 import type { Router } from 'express';
 
+import { adaptExpressRoute } from '@/main/adapters';
+import { makeCreateUserController } from '@/main/factories/application/controllers';
+
 export default (router: Router): void => {
-  router.get('/user/create', (req, res) => {
-    res.send({ data: 'any_data' });
-  });
+  router.post('/user/create', adaptExpressRoute(makeCreateUserController()));
 };
