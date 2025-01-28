76530cfde435c98933a4ffc58809b029d6f904cc
Allysson Alas <allyssonmgs@gmail.com>
2025-01-28 19:39:36 -0300

    test: ensure POST /user/create returns 403 if role does not exist

---
+++ tests/main/routes/user.test.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+import request from 'supertest';
+
+import { app } from '@/main/config/app';
+
+describe('User Routes', () => {
+  let prisma: PrismaClient;
+
+  beforeAll(() => {
+    prisma = new PrismaClient();
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany({});
+    await prisma.role.deleteMany({});
+    await prisma.permission.deleteMany({});
+  });
+
+  describe('POST /user/create', () => {
+    it('Should return 403 if role does not exist', async () => {
+      await request(app)
+        .post('/api/user/create')
+        .send({
+          name: 'Edmundo Girão',
+          email: 'ed_girao05@mail.com',
+          password: 'ed_gir@0.123',
+          role: 'admin',
+          contact: '(41) 99709-0876',
+          address: {
+            city: 'São Paulo',
+            neighborhood: 'Jardim Itapeva',
+            number: '08',
+            postalCode: '04674-070',
+            state: 'SP',
+            street: 'Rua General Antônio Tavares da Motta',
+            complement: '',
+          },
+        })
+        .expect(403);
+    });
+  });
+});
