f65acbcb3a6675972cb912ee0e2960d41f0178a0
Allysson Alas <allyssonmgs@gmail.com>
2025-02-03 22:06:06 -0300

    feat: ensure BcryptAdapter compare calls bcrypt compare with correct input

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -1,12 +1,16 @@
-import bcrypt from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 
-import type { HashGenerator } from '@/domain/contracts/gateways';
+import type { HashComparer, HashGenerator } from '@/domain/contracts/gateways';
 
 export class BcryptAdapter implements HashGenerator {
   constructor(private readonly salt: number) {}
 
   async generate({ plainText }: HashGenerator.Input): Promise<HashGenerator.Output> {
-    const cipherText = await bcrypt.hash(plainText, this.salt);
+    const cipherText = await hash(plainText, this.salt);
     return { cipherText };
+  }
+
+  async compare({ plainText, digest }: HashComparer.Input): Promise<void> {
+    await compare(plainText, digest);
   }
 }
