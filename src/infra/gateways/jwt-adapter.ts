f42235e3556c25984b52270f3546564806e1ef97
Allysson Alas <allyssonmgs@gmail.com>
2025-02-03 21:46:15 -0300

    feat: ensure JwtAdapter returns correct output on success

--- src/infra/gateways/jwt-adapter.ts
+++ src/infra/gateways/jwt-adapter.ts
@@ -2,10 +2,11 @@ import { sign } from 'jsonwebtoken';
 
 import type { JwtTokenGenerator } from '@/domain/contracts/gateways';
 
-export class JwtAdapter {
+export class JwtAdapter implements JwtTokenGenerator {
   constructor(private readonly secret: string) {}
 
-  async generate({ expirationInMs, ...input }: JwtTokenGenerator.Input): Promise<void> {
-    await sign(input, this.secret, { expiresIn: expirationInMs });
+  async generate({ expirationInMs, ...input }: JwtTokenGenerator.Input): Promise<JwtTokenGenerator.Output> {
+    const token = await sign(input, this.secret, { expiresIn: expirationInMs });
+    return { token };
   }
 }
