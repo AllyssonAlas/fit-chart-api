7dd70349e914b4e7f6403c76f9faf7b93f5f27ea
Allysson Alas <allyssonmgs@gmail.com>
2025-02-05 18:56:55 -0300

    feat: ensure AuthenticationController calls Authentication with correct input

--- src/application/controllers/authentication.ts
+++ src/application/controllers/authentication.ts
@@ -1,8 +1,20 @@
 import { Controller } from '@/application/controllers';
 import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
+import type { Authentication } from '@/domain/usecases';
+
+type Request = {
+  email: string;
+  password: string;
+};
 
 export class AuthenticationController extends Controller {
-  async perform(): Promise<any> {}
+  constructor(private readonly authentication: Authentication) {
+    super();
+  }
+
+  async perform(request: Request): Promise<any> {
+    await this.authentication(request);
+  }
 
   override buildValidators(request: any): Validator[] {
     // biome-ignore format: this array should not be formatted
