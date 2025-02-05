0a0c49245d14b0991fb7b17893c5e10a2d6238d0
Allysson Alas <allyssonmgs@gmail.com>
2025-02-05 19:02:54 -0300

    test: ensure AuthenticationController returns 401 if Authentication throws InvalidCredentialsError

--- tests/application/controllers/authentication.spec.ts
+++ tests/application/controllers/authentication.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationController, Controller } from '@/application/controllers';
 import { RequiredParam, RequiredPattern, RequiredString } from '@/application/validation';
+import { InvalidCredentialsError } from '@/domain/errors';
 
 describe('AuthenticationController', () => {
   let sut: AuthenticationController;
@@ -39,5 +40,16 @@ describe('AuthenticationController', () => {
 
     expect(authentication).toHaveBeenCalledWith(request);
     expect(authentication).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should return 401 if Authentication throws InvalidCredentialsError', async () => {
+    authentication.mockRejectedValueOnce(new InvalidCredentialsError());
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      data: new InvalidCredentialsError(),
+      statusCode: 401,
+    });
   });
 });
