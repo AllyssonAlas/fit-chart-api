23b5cb0dc4149406cfeb5b2f77c9ad895fc6d4fc
Allysson Alas <allyssonmgs@gmail.com>
2025-02-05 18:49:33 -0300

    test: ensure AuthenticationController calls Validators correctly

--- tests/application/controllers/authentication.spec.ts
+++ tests/application/controllers/authentication.spec.ts
@@ -1,7 +1,13 @@
 import { AuthenticationController, Controller } from '@/application/controllers';
+import { RequiredParam, RequiredPattern, RequiredString } from '@/application/validation';
 
 describe('AuthenticationController', () => {
   let sut: AuthenticationController;
+
+  const request = {
+    email: 'any_email@mail.com',
+    password: 'any_password',
+  };
 
   beforeEach(() => {
     sut = new AuthenticationController();
@@ -9,5 +15,17 @@ describe('AuthenticationController', () => {
 
   it('Should extend controller', () => {
     expect(sut).toBeInstanceOf(Controller);
+  });
+
+  it('Should build Validators correctly', () => {
+    const validators = sut.buildValidators(request);
+
+    expect(validators).toEqual([
+      new RequiredParam(request, 'email'),
+      new RequiredString(request.email, 'email'),
+      new RequiredPattern(request.email, 'email', /^[\w.]+@\w+.\w{2,}(?:.\w{2})?$/gim),
+      new RequiredParam(request, 'password'),
+      new RequiredString(request.password, 'password'),
+    ]);
   });
 });
