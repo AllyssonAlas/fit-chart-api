128877ac8a8064beea9863782bb0799b70e28d8c
Allysson Alas <allyssonmgs@gmail.com>
2025-02-05 18:49:21 -0300

    feat: ensure AuthenticationController calls Validators correctly

--- src/application/controllers/authentication.ts
+++ src/application/controllers/authentication.ts
@@ -1,5 +1,16 @@
 import { Controller } from '@/application/controllers';
+import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
 
 export class AuthenticationController extends Controller {
   async perform(): Promise<any> {}
+
+  override buildValidators(request: any): Validator[] {
+    // biome-ignore format: this array should not be formatted
+    return [
+        ...Builder.of(request)
+          .field('email').required().string().email()
+          .field('password').required().string()
+          .build(),
+      ];
+  }
 }
