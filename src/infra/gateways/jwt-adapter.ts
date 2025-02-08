85053930ce6b0407321a0695ff62bdf13e97f347
Allysson Alas <allyssonmgs@gmail.com>
2025-02-08 16:03:26 -0300

    feat: ensure JwtAdapter.validate() calls jsonwebtoken.verify() with correct input

--- src/infra/gateways/jwt-adapter.ts
+++ src/infra/gateways/jwt-adapter.ts
@@ -1,12 +1,16 @@
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
-import type { JwtTokenGenerator } from '@/domain/contracts/gateways';
+import type { JwtTokenGenerator, JwtTokenValidator } from '@/domain/contracts/gateways';
 
 export class JwtAdapter implements JwtTokenGenerator {
   constructor(private readonly secret: string) {}
 
   async generate({ expirationInMs, ...input }: JwtTokenGenerator.Input): Promise<JwtTokenGenerator.Output> {
-    const token = await sign(input, this.secret, { expiresIn: expirationInMs });
+    const token = sign(input, this.secret, { expiresIn: expirationInMs });
     return { token };
+  }
+
+  async validate({ token }: JwtTokenValidator.Input): Promise<void> {
+    verify(token, this.secret);
   }
 }
