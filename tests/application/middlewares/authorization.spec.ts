a888446b7f31cae609012c2ab20386d817f47bb4
Allysson Alas <allyssonmgs@gmail.com>
2025-02-10 20:18:00 -0300

    test: ensure AuthorizationMiddleware.handle() return 401 if authorization is not provided

---
+++ tests/application/middlewares/authorization.spec.ts
@@ -0,0 +1,19 @@
+import { UnauthorizedError } from '@/application/errors';
+import { AuthorizationMiddleware } from '@/application/middlewares';
+
+describe('AuthorizationMiddleware', () => {
+  let sut: AuthorizationMiddleware;
+
+  beforeEach(() => {
+    sut = new AuthorizationMiddleware();
+  });
+
+  it('Should return 401 if authorization is not provided', async () => {
+    const httpResponse = await sut.handle({} as any);
+
+    expect(httpResponse).toEqual({
+      statusCode: 401,
+      data: new UnauthorizedError(),
+    });
+  });
+});
