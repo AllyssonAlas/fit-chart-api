e83df6949806f1bfb72655469f9e5797ee059858
Allysson Alas <allyssonmgs@gmail.com>
2025-02-10 20:23:28 -0300

    feat: ensure AuthorizationMiddleware calls Authorization with correct input

--- src/application/middlewares/authorization.ts
+++ src/application/middlewares/authorization.ts
@@ -1,12 +1,19 @@
 import { UnauthorizedError } from '@/application/errors';
 import { type HttpResponse, unauthorized } from '@/application/helpers';
+import type { Authorization } from '@/domain/usecases';
 
 type HttpRequest = {
   authorization: string;
 };
 
 export class AuthorizationMiddleware {
-  async handle(request: HttpRequest): Promise<HttpResponse<Error>> {
-    return unauthorized(new UnauthorizedError());
+  constructor(
+    private readonly authorize: Authorization,
+    private readonly requiredPermission: string,
+  ) {}
+
+  async handle({ authorization: authToken }: HttpRequest): Promise<HttpResponse<Error> | void> {
+    if (!authToken) return unauthorized(new UnauthorizedError());
+    await this.authorize({ authToken, requiredPermission: this.requiredPermission });
   }
 }
