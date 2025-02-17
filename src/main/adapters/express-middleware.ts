d0c10dac71a89235d9c21c2bf3045046bd87fbe2
Allysson Alas <allyssonmgs@gmail.com>
2025-02-17 20:12:31 -0300

    feat: ensure ExpressMiddleware returns same error as Middleware.handle()

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -3,7 +3,8 @@ import type { RequestHandler } from 'express';
 import type { Middleware } from '@/application/contracts';
 
 export const adaptExpressMiddleware = (middleware: Middleware): RequestHandler => {
-  return async (req) => {
-    await middleware.handle({ ...req.headers });
+  return async (req, res) => {
+    const { data, statusCode } = await middleware.handle({ ...req.headers });
+    res.status(statusCode).json({ error: data.message });
   };
 };
