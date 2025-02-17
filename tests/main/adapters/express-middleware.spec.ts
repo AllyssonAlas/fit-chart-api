7dfe01d63fa7b4ef3051205239a8d4217153bb83
Allysson Alas <allyssonmgs@gmail.com>
2025-02-17 20:12:41 -0300

    test: ensure ExpressMiddleware returns same error as Middleware.handle()

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -1,5 +1,5 @@
-import { getMockReq } from '@jest-mock/express';
-import type { Request } from 'express';
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import type { Request, Response } from 'express';
 import { type MockProxy, mock } from 'jest-mock-extended';
 
 import type { Middleware } from '@/application/contracts';
@@ -9,12 +9,23 @@ describe('ExpressMiddleware', () => {
   let sut: any;
   let middleware: MockProxy<Middleware>;
   let req: Request;
+  let res: Response;
 
   const requestHeaders = { any: 'any' };
 
   beforeAll(() => {
     req = getMockReq({ headers: requestHeaders });
+    res = getMockRes().res;
     middleware = mock();
+    middleware.handle.mockResolvedValue({
+      statusCode: 200,
+      data: {
+        emptyProp: '',
+        nullProp: null,
+        undefinedProp: undefined,
+        prop: 'any_value',
+      },
+    });
   });
 
   beforeEach(() => {
@@ -22,7 +33,7 @@ describe('ExpressMiddleware', () => {
   });
 
   it('Should call handle with correct request', async () => {
-    await sut(req);
+    await sut(req, res);
 
     expect(middleware.handle).toHaveBeenCalledWith(requestHeaders);
     expect(middleware.handle).toHaveBeenCalledTimes(1);
@@ -31,9 +42,21 @@ describe('ExpressMiddleware', () => {
   it('Should call handle with empty request', async () => {
     req = getMockReq();
 
-    await sut(req);
+    await sut(req, res);
 
     expect(middleware.handle).toHaveBeenCalledWith({});
     expect(middleware.handle).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should respond with correct error and statusCode', async () => {
+    const error = new Error('any_error');
+    middleware.handle.mockResolvedValueOnce({ statusCode: 500, data: error });
+
+    await sut(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ error: error.message });
+    expect(res.json).toHaveBeenCalledTimes(1);
   });
 });
