3c745ad31aedabb487a1e7a50fed85918070946c
Allysson Alas <allyssonmgs@gmail.com>
2025-02-17 19:49:52 -0300

    test: ensure ExpressMiddleware calls Middleware.handle() with correct request

---
+++ tests/main/adapters/express-middleware.spec.ts
@@ -0,0 +1,30 @@
+import { getMockReq } from '@jest-mock/express';
+import type { Request } from 'express';
+import { type MockProxy, mock } from 'jest-mock-extended';
+
+import type { Middleware } from '@/application/contracts';
+import { adaptExpressMiddleware } from '@/main/adapters';
+
+describe('ExpressMiddleware', () => {
+  let sut: any;
+  let middleware: MockProxy<Middleware>;
+  let req: Request;
+
+  const requestHeaders = { any: 'any' };
+
+  beforeAll(() => {
+    req = getMockReq({ headers: requestHeaders });
+    middleware = mock();
+  });
+
+  beforeEach(() => {
+    sut = adaptExpressMiddleware(middleware);
+  });
+
+  it('Should call handle with correct request', async () => {
+    await sut(req);
+
+    expect(middleware.handle).toHaveBeenCalledWith(requestHeaders);
+    expect(middleware.handle).toHaveBeenCalledTimes(1);
+  });
+});
