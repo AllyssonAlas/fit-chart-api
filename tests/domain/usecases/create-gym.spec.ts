bf19f961011e6e418833615a6ae665560e4c031f
Allysson Alas <allyssonmgs@gmail.com>
2025-02-20 12:39:30 -0300

    test: ensure CreateGym calls LoadUserRepository with correct input

---
+++ tests/domain/usecases/create-gym.spec.ts
@@ -0,0 +1,41 @@
+import { type MockProxy, mock } from 'jest-mock-extended';
+
+import type { LoadUserRepository } from '@/domain/contracts/repositories';
+import { type CreateGym, setupCreateGym } from '@/domain/usecases';
+
+describe('CreateGym', () => {
+  const input = {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    contact: 'any_contact',
+    ownerEmail: 'any_owner_email@mail.com',
+    administrators: ['any_admin_email_1@mail.com', 'any_admin_email_2@mail.com'],
+    address: {
+      city: 'any_city',
+      neighborhood: 'any_neighborhood',
+      number: 'any_number',
+      postalCode: 'any_postal_code',
+      state: 'any_state',
+      street: 'any_street',
+      complement: 'any_complement',
+    },
+  };
+
+  let sut: CreateGym;
+  let userRepository: MockProxy<LoadUserRepository>;
+
+  beforeAll(() => {
+    userRepository = mock();
+  });
+
+  beforeEach(() => {
+    sut = setupCreateGym(userRepository);
+  });
+
+  it('Should call LoadUserRepository with correct input', async () => {
+    await sut(input);
+
+    expect(userRepository.load).toHaveBeenCalledWith({ email: input.ownerEmail });
+    expect(userRepository.load).toHaveBeenCalledTimes(1);
+  });
+});
