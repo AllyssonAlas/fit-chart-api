c32b30107b5b506794f959ef6bebfa1e072a7f9f
Allysson Alas <allyssonmgs@gmail.com>
2025-02-26 19:25:37 -0300

    feat: add Gym schema

---
+++ src/infra/database/postgres/prisma/migrations/20250226223156_create_gym/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "Gym" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "ownerEmail" TEXT NOT NULL,
+    "administrators" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Gym_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GymAddress" (
+    "id" TEXT NOT NULL,
+    "gymId" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" VARCHAR(2) NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "complement" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "GymAddress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GymAddress_gymId_key" ON "GymAddress"("gymId");
+
+-- AddForeignKey
+ALTER TABLE "GymAddress" ADD CONSTRAINT "GymAddress_gymId_fkey" FOREIGN KEY ("gymId") REFERENCES "Gym"("id") ON DELETE CASCADE ON UPDATE CASCADE;
