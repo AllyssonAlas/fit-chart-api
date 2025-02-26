8746e5128032300dbcb2db990b8f934815e2e2ab
Allysson Alas <allyssonmgs@gmail.com>
2025-02-26 20:55:47 -0300

    feat: ensure GymRepository.save() creates a Gym with correct input

---
+++ src/infra/database/postgres/repositories/gym.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+import type { SaveGymRepository } from '@/domain/contracts/repositories';
+
+export class GymRepository implements SaveGymRepository {
+  async save(input: SaveGymRepository.Input): Promise<SaveGymRepository.Output> {
+    const prisma = new PrismaClient();
+    await prisma.gym.create({
+      data: { ...input, address: { create: input.address } },
+    });
+  }
+}
