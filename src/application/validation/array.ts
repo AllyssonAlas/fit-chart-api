99ea2b0a189bfebf3591da405eeeab1380f04b84
Allysson Alas <allyssonmgs@gmail.com>
2025-03-01 17:00:22 -0300

    feat: ensure RequiredArray returns InvalidParamError if field value is not an array

---
+++ src/application/validation/array.ts
@@ -0,0 +1,13 @@
+import { InvalidParamError } from '@/application/errors';
+import type { Validator } from '@/application/validation';
+
+export class RequiredArray implements Validator {
+  constructor(
+    readonly value: any,
+    readonly fieldName: string,
+  ) {}
+
+  validate(): Error {
+    return new InvalidParamError(this.fieldName);
+  }
+}
