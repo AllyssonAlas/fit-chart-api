6533f3c2132a3fa179f89b2edf25da2043fab579
Allysson Alas <allyssonmgs@gmail.com>
2025-03-06 19:35:18 -0300

    test: ensure POST /gym returns 403 if ownerEmail does not exist

--- tests/main/routes/gym.test.ts
+++ tests/main/routes/gym.test.ts
@@ -4,6 +4,7 @@ import request from 'supertest';
 import { AuthToken } from '@/domain/entities';
 import { app } from '@/main/config/app';
 import { env } from '@/main/config/env';
+import { Permissions } from '@/main/enums';
 
 describe('Gym Routes', () => {
   describe('POST /gym', () => {
@@ -30,6 +31,33 @@ describe('Gym Routes', () => {
     it('Should return 403 if authorization token does not contain required permission', async () => {
       const authorizationToken = sign(
         { id: 'any_user_id', role: 'any_role_name', permissions: ['invalid_permission'] },
+        env.secret,
+        { expiresIn: AuthToken.expirationInMs / 1000 },
+      );
+
+      await request(app)
+        .post('/api/gym')
+        .set('authorization', authorizationToken)
+        .send({
+          name: 'Baroque Workout',
+          email: 'baroque_workout@mail.com',
+          contact: 'baroque_workout@mail.com',
+          ownerEmail: 'crocodile_mr0@mail.com',
+          address: {
+            city: 'São Paulo',
+            neighborhood: 'Jardim Itapeva',
+            number: '08',
+            postalCode: '04674-070',
+            state: 'SP',
+            street: 'Rua General Antônio Tavares da Motta',
+          },
+        })
+        .expect(403);
+    });
+
+    it('Should return 403 if ownerEmail does not exist', async () => {
+      const authorizationToken = sign(
+        { id: 'any_user_id', role: 'any_role_name', permissions: [Permissions.CREATE_GYM] },
         env.secret,
         { expiresIn: AuthToken.expirationInMs / 1000 },
       );
