cadfc403385d6aa3571fa0eb96f0368e3a30eb76
Allysson Alas <allyssonmgs@gmail.com>
2025-03-06 19:34:32 -0300

    test: ensure POST /gym returns 403 if authorization token does not contain required permission

--- tests/main/routes/gym.test.ts
+++ tests/main/routes/gym.test.ts
@@ -1,6 +1,9 @@
+import { sign } from 'jsonwebtoken';
 import request from 'supertest';
 
+import { AuthToken } from '@/domain/entities';
 import { app } from '@/main/config/app';
+import { env } from '@/main/config/env';
 
 describe('Gym Routes', () => {
   describe('POST /gym', () => {
@@ -22,6 +25,33 @@ describe('Gym Routes', () => {
           },
         })
         .expect(401);
+    });
+
+    it('Should return 403 if authorization token does not contain required permission', async () => {
+      const authorizationToken = sign(
+        { id: 'any_user_id', role: 'any_role_name', permissions: ['invalid_permission'] },
+        env.secret,
+        { expiresIn: AuthToken.expirationInMs / 1000 },
+      );
+
+      await request(app)
+        .post('/api/gym')
+        .set('authorization', authorizationToken)
+        .send({
+          name: 'Baroque Workout',
+          email: 'baroque_workout@mail.com',
+          contact: 'baroque_workout@mail.com',
+          ownerEmail: 'crocodile_mr0@mail.com',
+          address: {
+            city: 'São Paulo',
+            neighborhood: 'Jardim Itapeva',
+            number: '08',
+            postalCode: '04674-070',
+            state: 'SP',
+            street: 'Rua General Antônio Tavares da Motta',
+          },
+        })
+        .expect(403);
     });
   });
 });
