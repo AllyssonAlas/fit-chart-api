0a51a855bd7bab8135f4486703db8d041e91a0c7
Allysson Alas <allyssonmgs@gmail.com>
2025-03-12 19:58:54 -0300

    refactor: apply ForbidRoleCreationDecorator on route POST /user

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,11 @@
 import type { Router } from 'express';
 
 import { adaptExpressRoute } from '@/main/adapters';
+import { Roles } from '@/main/enums';
 import { makeAuthenticationController, makeCreateUserController } from '@/main/factories/application/controllers';
+import { makeForbidRoleCreationDecorator } from '@/main/factories/main/decorators';
 
 export default (router: Router): void => {
-  router.post('/user', adaptExpressRoute(makeCreateUserController()));
+  router.post('/user', adaptExpressRoute(makeForbidRoleCreationDecorator([Roles.ADMIN])));
   router.post('/login', adaptExpressRoute(makeAuthenticationController()));
 };
