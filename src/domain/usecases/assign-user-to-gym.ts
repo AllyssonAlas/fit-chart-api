46009c15479daf1827974d5256a51b8a8d919f75
Allysson Alas <allyssonmgs@gmail.com>
2025-03-19 10:09:51 -0300

    feat: ensure AssignUserToGym calls LoadManyUsersRepository with correct input

--- src/domain/usecases/assign-user-to-gym.ts
+++ src/domain/usecases/assign-user-to-gym.ts
@@ -1,14 +1,15 @@
-import type { LoadGymRepository } from '@/domain/contracts/repositories';
+import type { LoadGymRepository, LoadManyUsersRepository } from '@/domain/contracts/repositories';
 import { GymNotFoundError } from '@/domain/errors';
 
 type Input = { gymId: string; usersType: string; usersEmails: string[] };
 type Output = void;
 export type AssignUserToGym = (input: Input) => Promise<Output>;
-type Setup = (gymRepository: LoadGymRepository) => AssignUserToGym;
+type Setup = (gymRepository: LoadGymRepository, userRepository: LoadManyUsersRepository) => AssignUserToGym;
 
-export const setupAssignUserToGym: Setup = (gymRepository) => {
-  return async ({ gymId }) => {
+export const setupAssignUserToGym: Setup = (gymRepository, userRepository) => {
+  return async ({ gymId, usersEmails }) => {
     const gymData = await gymRepository.load({ id: gymId });
     if (!gymData) throw new GymNotFoundError();
+    await userRepository.loadMany({ emails: usersEmails });
   };
 };
