76c08363e0f7681ef952e516fab800a4072ff43b
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 09:31:06 -0300

    test: ensure AssignUserToGymController builds validators correctly

--- tests/application/controllers/assign-user-to-gym.spec.ts
+++ tests/application/controllers/assign-user-to-gym.spec.ts
@@ -1,8 +1,15 @@
 import { AssignUserToGymController, Controller } from '@/application/controllers';
+import { RequiredArray, RequiredParam, RequiredString, RequiredStringArray } from '@/application/validation';
 
 jest.mock('@/application/validation/composite');
 
 describe('AssignUserToGymController', () => {
+  const request = {
+    gymId: 'any_gym_id',
+    usersEmails: ['any_email_1@mail.com', 'any_email_2@mail.com', 'any_email_3@mail.com'],
+    usersType: 'students',
+  };
+
   let sut: AssignUserToGymController;
 
   beforeEach(() => {
@@ -11,5 +18,19 @@ describe('AssignUserToGymController', () => {
 
   it('Should extend controller', () => {
     expect(sut).toBeInstanceOf(Controller);
+  });
+
+  it('Should build Validators correctly', () => {
+    const validators = sut.buildValidators(request);
+
+    expect(validators).toEqual([
+      new RequiredParam(request, 'gymId'),
+      new RequiredString(request.gymId, 'gymId'),
+      new RequiredParam(request, 'usersEmails'),
+      new RequiredArray(request.usersEmails, 'usersEmails'),
+      new RequiredStringArray(request.usersEmails, 'usersEmails'),
+      new RequiredParam(request, 'usersType'),
+      new RequiredString(request.usersType, 'usersType'),
+    ]);
   });
 });
