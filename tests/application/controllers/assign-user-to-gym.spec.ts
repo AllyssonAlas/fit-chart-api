ebc90871d22c880c61eee23c1a35a9d2475494e3
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 10:24:37 -0300

    test: ensure AssignUserToGymController returns 404 if AssignUsersToGym throws GymNotFoundError

--- tests/application/controllers/assign-user-to-gym.spec.ts
+++ tests/application/controllers/assign-user-to-gym.spec.ts
@@ -1,6 +1,7 @@
 import { AssignUserToGymController, Controller } from '@/application/controllers';
 import { ServerError } from '@/application/errors';
 import { RequiredArray, RequiredParam, RequiredString, RequiredStringArray } from '@/application/validation';
+import { GymNotFoundError } from '@/domain/errors';
 
 jest.mock('@/application/validation/composite');
 
@@ -45,6 +46,18 @@ describe('AssignUserToGymController', () => {
 
     expect(assignUserToGym).toHaveBeenCalledWith(request);
     expect(assignUserToGym).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should return 404 if AssignUserToGym throws GymNotFoundError', async () => {
+    const error = new GymNotFoundError();
+    assignUserToGym.mockRejectedValueOnce(error);
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      data: error,
+      statusCode: 404,
+    });
   });
 
   it('Should return 500 if AssignUserToGym throws infra error', async () => {
