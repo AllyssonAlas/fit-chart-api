891889a3ed81b0c5bb85e91fec28a547104a4c23
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 09:16:31 -0300

    feat: ensure AssignUserToGymController extends Controller

---
+++ src/application/controllers/assign-user-to-gym.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@/application/controllers';
+
+export class AssignUserToGymController extends Controller {
+  async perform(): Promise<any> {}
+}
--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from './controller'; // the abstract class shall come first
+export * from './assign-user-to-gym';
 export * from './authentication';
 export * from './create-gym';
 export * from './create-user';
