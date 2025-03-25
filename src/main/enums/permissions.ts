d4f16488fc1c2a5e7e2f34929f70c816b80b93c8
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 19:46:37 -0300

    feat: add ASSiGN_USER_TO_GYM permission

--- src/main/enums/permissions.ts
+++ src/main/enums/permissions.ts
@@ -2,6 +2,7 @@ import { Roles } from './roles';
 
 export enum Permissions {
   CREATE_GYM = 'create-gym',
+  ASSiGN_USER_TO_GYM = 'assign-user-to-gym',
 }
 
 type PermissionList = {
@@ -9,4 +10,7 @@ type PermissionList = {
   roles: Roles[];
 }[];
 
-export const permissionsList: PermissionList = [{ name: Permissions.CREATE_GYM, roles: [Roles.ADMIN] }];
+export const permissionsList: PermissionList = [
+  { name: Permissions.CREATE_GYM, roles: [Roles.ADMIN] },
+  { name: Permissions.ASSiGN_USER_TO_GYM, roles: [Roles.ADMIN] },
+];
