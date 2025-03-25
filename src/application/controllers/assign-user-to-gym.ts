e770e8021a50dc863cf5ac1c43b6ec4d96c77945
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 10:30:32 -0300

    feat: ensure AssignUserToGymController returns 204 on success

--- src/application/controllers/assign-user-to-gym.ts
+++ src/application/controllers/assign-user-to-gym.ts
@@ -1,19 +1,22 @@
 import { Controller } from '@/application/controllers';
-import { forbidden, notFound } from '@/application/helpers';
+import { type HttpResponse, forbidden, noContent, notFound } from '@/application/helpers';
 import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
 import { EmailDoesNotExistError, GymNotFoundError } from '@/domain/errors';
 import type { AssignUsersToGym } from '@/domain/usecases';
 
 type Request = { gymId: string; usersType: string; usersEmails: string[] };
 
+type Model = null | Error;
+
 export class AssignUserToGymController extends Controller {
   constructor(private readonly assignUsersToGym: AssignUsersToGym) {
     super();
   }
 
-  async perform(request: Request): Promise<any> {
+  async perform(request: Request): Promise<HttpResponse<Model>> {
     try {
       await this.assignUsersToGym(request);
+      return noContent();
     } catch (error) {
       if (error instanceof GymNotFoundError) {
         return notFound(error);
