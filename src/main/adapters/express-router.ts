e023a9813b0430b8005d0c3e8b79e0dc04f67940
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 14:36:54 -0300

    feat: ensure adaptExpressRoute calls Controller.handle with correct request params

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,7 +4,7 @@ import type { Controller } from '@/application/controllers';
 
 export const adaptExpressRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const { data, statusCode } = await controller.handle(req.body);
+    const { data, statusCode } = await controller.handle({ ...req.body, ...req.params });
     const json = [200, 204].includes(statusCode) ? data : { error: data.message };
     res.status(statusCode).json(json);
   };
