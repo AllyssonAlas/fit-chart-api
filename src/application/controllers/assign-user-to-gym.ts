e98e169f811aaaffa70bad7634739deee30a4f00
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 09:30:54 -0300

    feat: ensure AssignUserToGymController builds validators correctly

--- src/application/controllers/assign-user-to-gym.ts
+++ src/application/controllers/assign-user-to-gym.ts
@@ -1,5 +1,17 @@
 import { Controller } from '@/application/controllers';
+import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
 
 export class AssignUserToGymController extends Controller {
   async perform(): Promise<any> {}
+
+  override buildValidators(request: any): Validator[] {
+    // biome-ignore format: this array should not be formatted
+    return [
+      ...Builder.of(request)
+      .field('gymId').string()
+      .field('usersEmails').array().stringArray()
+      .field('usersType').string()
+      .build(),
+    ];
+  }
 }
