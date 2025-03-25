8e9630942157b2004f72b4961ce6d06bb48b6fa9
Allysson Alas <allyssonmgs@gmail.com>
2025-03-25 09:56:53 -0300

    feat: ensure AssignUserToGymController calls AssignUsersToGym with correct input

--- src/application/controllers/assign-user-to-gym.ts
+++ src/application/controllers/assign-user-to-gym.ts
@@ -1,8 +1,17 @@
 import { Controller } from '@/application/controllers';
 import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
+import type { AssignUsersToGym } from '@/domain/usecases';
+
+type Request = { gymId: string; usersType: string; usersEmails: string[] };
 
 export class AssignUserToGymController extends Controller {
-  async perform(): Promise<any> {}
+  constructor(private readonly assignUsersToGym: AssignUsersToGym) {
+    super();
+  }
+
+  async perform(request: Request): Promise<any> {
+    await this.assignUsersToGym(request);
+  }
 
   override buildValidators(request: any): Validator[] {
     // biome-ignore format: this array should not be formatted
