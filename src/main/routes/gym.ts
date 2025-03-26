22b2b4879d032be4437e9f353b0e9a11f9f17d73
Allysson Alas <allyssonmgs@gmail.com>
2025-03-26 15:16:39 -0300

    feat: add PUT /gym/:gymId/assign route

--- src/main/routes/gym.ts
+++ src/main/routes/gym.ts
@@ -2,11 +2,16 @@ import type { Router } from 'express';
 
 import { adaptExpressMiddleware as adaptMiddleware, adaptExpressRoute as adaptRoute } from '@/main/adapters';
 import { Permissions } from '@/main/enums';
-import { makeCreateGymController } from '@/main/factories/application/controllers';
+import { makeAssignUsersToGymController, makeCreateGymController } from '@/main/factories/application/controllers';
 import { makeAuthorizationMiddleware } from '@/main/factories/application/middlewares';
 
-const { CREATE_GYM } = Permissions;
+const { CREATE_GYM, ASSiGN_USER_TO_GYM } = Permissions;
 
 export default (router: Router): void => {
   router.post('/gym', adaptMiddleware(makeAuthorizationMiddleware(CREATE_GYM)), adaptRoute(makeCreateGymController()));
+  router.put(
+    '/gym/:gymId/assign',
+    adaptMiddleware(makeAuthorizationMiddleware(ASSiGN_USER_TO_GYM)),
+    adaptRoute(makeAssignUsersToGymController()),
+  );
 };
