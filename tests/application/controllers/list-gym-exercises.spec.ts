21158dd97f5f2b377ec08bd009159f58a98d00f3
Allysson Alas <allyssonmgs@gmail.com>
2025-04-05 21:22:15 -0300

    test: ensure ListGymExercisesController calls LoadGymExercisesRepository with correct input

--- tests/application/controllers/list-gym-exercises.spec.ts
+++ tests/application/controllers/list-gym-exercises.spec.ts
@@ -1,13 +1,32 @@
+import { type MockProxy, mock } from 'jest-mock-extended';
+
 import { Controller, ListGymExercisesController } from '@/application/controllers';
+import type { LoadGymExercisesRepository } from '@/domain/contracts/repositories';
 
 describe('ListGymExercisesController', () => {
+  const request = {
+    gymId: 'any_gym_id',
+  };
+
   let sut: ListGymExercisesController;
+  let gymRepository: MockProxy<LoadGymExercisesRepository>;
+
+  beforeAll(() => {
+    gymRepository = mock();
+  });
 
   beforeEach(() => {
-    sut = new ListGymExercisesController();
+    sut = new ListGymExercisesController(gymRepository);
   });
 
   it('Should extend controller', () => {
     expect(sut).toBeInstanceOf(Controller);
+  });
+
+  it('Should call LoadGymExercisesRepository with correct input', async () => {
+    await sut.handle(request);
+
+    expect(gymRepository.loadExercises).toHaveBeenCalledWith(request);
+    expect(gymRepository.loadExercises).toHaveBeenCalledTimes(1);
   });
 });
