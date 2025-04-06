82a35d47530d9f97aee7758b3cc00d666e651cf2
Allysson Alas <allyssonmgs@gmail.com>
2025-04-05 21:15:07 -0300

    feat: ensure ListGymExercisesController extends Controller

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './assign-users-to-gym';
 export * from './authentication';
 export * from './create-gym';
 export * from './create-user';
+export * from './list-gym-exercises';
---
+++ src/application/controllers/list-gym-exercises.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@/application/controllers';
+
+export class ListGymExercisesController extends Controller {
+  async perform(): Promise<any> {}
+}
