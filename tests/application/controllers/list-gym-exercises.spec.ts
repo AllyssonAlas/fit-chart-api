9dd19240279ef781e8cf48c9cb55826e85814246
Allysson Alas <allyssonmgs@gmail.com>
2025-04-05 21:29:19 -0300

    test: ensure ListGymExercisesController returns 204 on empty exercise list

--- tests/application/controllers/list-gym-exercises.spec.ts
+++ tests/application/controllers/list-gym-exercises.spec.ts
@@ -42,4 +42,15 @@ describe('ListGymExercisesController', () => {
       statusCode: 500,
     });
   });
+
+  it('Should return 204 if LoadGymExercisesRepository returns an empty list', async () => {
+    gymRepository.loadExercises.mockResolvedValueOnce([]);
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      data: null,
+      statusCode: 204,
+    });
+  });
 });
