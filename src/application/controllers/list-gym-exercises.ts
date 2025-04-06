1ddb7ea7349a49a0aa23e35e58d99a1e94eec07c
Allysson Alas <allyssonmgs@gmail.com>
2025-04-05 21:22:04 -0300

    feat: ensure ListGymExercisesController calls LoadGymExercisesRepository with correct input

--- src/application/controllers/list-gym-exercises.ts
+++ src/application/controllers/list-gym-exercises.ts
@@ -1,5 +1,16 @@
 import { Controller } from '@/application/controllers';
+import type { LoadGymExercisesRepository } from '@/domain/contracts/repositories';
+
+type Request = {
+  gymId: string;
+};
 
 export class ListGymExercisesController extends Controller {
-  async perform(): Promise<any> {}
+  constructor(private readonly gymRepository: LoadGymExercisesRepository) {
+    super();
+  }
+
+  async perform(request: Request): Promise<any> {
+    await this.gymRepository.loadExercises(request);
+  }
 }
