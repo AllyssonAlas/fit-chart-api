ad919f0e9965b7170c3c923228efc666918a16e4
Allysson Alas <allyssonmgs@gmail.com>
2025-05-29 18:17:08 -0300

    feat: ensure Validators are build correctly

--- src/application/controllers/create-exercises-chart.ts
+++ src/application/controllers/create-exercises-chart.ts
@@ -1,5 +1,35 @@
 import { Controller } from '@/application/controllers';
+import { ValidationBuilder as Builder, type Validator } from '@/application/validation';
+import type { SaveExercisesChartRepository } from '@/domain/contracts/repositories';
+
+type Request = SaveExercisesChartRepository.Input;
 
 export class CreateExercisesChartController extends Controller {
   async perform(request: Request): Promise<any> {}
+
+  override buildValidators(request: Request): Validator[] {
+    // biome-ignore format: this array should not be formatted
+    return [
+      ...Builder.of(request)
+        .field('userId').string()
+        .field('goals').string()
+        .field('observation').string().optional()
+        .field('divisions').array()
+        .field('exercises').array()
+        .build(),
+      ...request.divisions.flatMap((_, index) => [
+        ...Builder.of(request.divisions[index])
+          .subField('name', 'divisions').string()
+          .subField('weekDays', 'divisions').array().numberArray().build(),
+      ]),
+        ...request.exercises.flatMap((_, index) => [
+        ...Builder.of(request.exercises[index])
+          .subField('exerciseId', 'exercises').string()
+          .subField('series', 'exercises').number()
+          .subField('repts', 'exercises').number()
+          .subField('weight', 'exercises').number()
+          .subField('division', 'exercises').string().build(),
+      ]),
+    ];
+  }
 }
