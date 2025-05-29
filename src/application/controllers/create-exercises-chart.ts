70dc5ab510e641b8d7cace8a3526696addeee30b
Allysson Alas <allyssonmgs@gmail.com>
2025-05-29 18:12:55 -0300

    feat: ensure CreateExercisesChartController extends Controller

---
+++ src/application/controllers/create-exercises-chart.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@/application/controllers';
+
+export class CreateExercisesChartController extends Controller {
+  async perform(request: Request): Promise<any> {}
+}
--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -2,5 +2,6 @@ export * from './controller'; // the abstract class shall come first
 export * from './assign-users-to-gym';
 export * from './authentication';
 export * from './create-gym';
+export * from './create-exercises-chart';
 export * from './create-user';
 export * from './list-gym-exercises';
