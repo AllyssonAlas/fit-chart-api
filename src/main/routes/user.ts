f43c78bf2faf3c65c1b59d8701ee6faed0ee5fa8
Allysson Alas <allyssonmgs@gmail.com>
2025-06-01 16:19:16 -0300

    refactor: remove unused import

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -2,7 +2,7 @@ import type { Router } from 'express';
 
 import { adaptExpressRoute } from '@/main/adapters';
 import { Roles } from '@/main/enums';
-import { makeAuthenticationController, makeCreateUserController } from '@/main/factories/application/controllers';
+import { makeAuthenticationController } from '@/main/factories/application/controllers';
 import { makeForbidRoleCreationDecorator } from '@/main/factories/main/decorators';
 
 export default (router: Router): void => {
