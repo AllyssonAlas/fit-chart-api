fa114e03fe340f830911ad3798fe1897ab2dbfe6
Allysson Alas <allyssonmgs@gmail.com>
2025-06-02 19:50:13 -0300

    feat: add POST /user/:userId/exercisesChart

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,11 +1,22 @@
 import type { Router } from 'express';
 
-import { adaptExpressRoute } from '@/main/adapters';
-import { Roles } from '@/main/enums';
-import { makeAuthenticationController } from '@/main/factories/application/controllers';
+import { adaptExpressMiddleware, adaptExpressRoute } from '@/main/adapters';
+import { Permissions, Roles } from '@/main/enums';
+import {
+  makeAuthenticationController,
+  makeCreateExercisesChartController,
+} from '@/main/factories/application/controllers';
+import { makeAuthorizationMiddleware } from '@/main/factories/application/middlewares';
 import { makeForbidRoleCreationDecorator } from '@/main/factories/main/decorators';
+
+const { CREATE_EXERCISES_CHART } = Permissions;
 
 export default (router: Router): void => {
   router.post('/user', adaptExpressRoute(makeForbidRoleCreationDecorator([Roles.ADMIN])));
   router.post('/login', adaptExpressRoute(makeAuthenticationController()));
+  router.post(
+    '/user/:userId/exercisesChart',
+    adaptExpressMiddleware(makeAuthorizationMiddleware(CREATE_EXERCISES_CHART)),
+    adaptExpressRoute(makeCreateExercisesChartController()),
+  );
 };
