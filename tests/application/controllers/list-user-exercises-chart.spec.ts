4d750ee010d9f6fcdef30226046d8355b2ec1bcb
Allysson Alas <allyssonmgs@gmail.com>
2025-06-04 19:41:10 -0300

    test: ensure ListUserExercisesChartsController calls LoadUserExercisesChartsRepo with correct input

--- tests/application/controllers/list-user-exercises-chart.spec.ts
+++ tests/application/controllers/list-user-exercises-chart.spec.ts
@@ -1,13 +1,32 @@
+import { type MockProxy, mock } from 'jest-mock-extended';
+
 import { Controller, ListUserExercisesChartsController } from '@/application/controllers';
+import type { LoadUserExercisesChartsRepository } from '@/domain/contracts/repositories';
 
 describe('ListUserExercisesChartsController', () => {
+  const request = {
+    userId: 'any_user_id',
+  };
+
   let sut: ListUserExercisesChartsController;
+  let exercisesChartsRepository: MockProxy<LoadUserExercisesChartsRepository>;
+
+  beforeAll(() => {
+    exercisesChartsRepository = mock();
+  });
 
   beforeEach(() => {
-    sut = new ListUserExercisesChartsController();
+    sut = new ListUserExercisesChartsController(exercisesChartsRepository);
   });
 
   it('Should extend controller', () => {
     expect(sut).toBeInstanceOf(Controller);
+  });
+
+  it('Should call LoadGymExercisesRepository with correct input', async () => {
+    await sut.handle(request);
+
+    expect(exercisesChartsRepository.loadExercisesCharts).toHaveBeenCalledWith(request);
+    expect(exercisesChartsRepository.loadExercisesCharts).toHaveBeenCalledTimes(1);
   });
 });
