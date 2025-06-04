80d5552c84e7174437e61f3013044e2a5dbfa280
Allysson Alas <allyssonmgs@gmail.com>
2025-06-04 20:01:10 -0300

    feat: ensure ListUserExercisesChartsController 200 with correct output on success

--- src/application/controllers/list-user-exercises-chart.ts
+++ src/application/controllers/list-user-exercises-chart.ts
@@ -1,18 +1,20 @@
 import { Controller } from '@/application/controllers';
-import { type HttpResponse, noContent } from '@/application/helpers';
+import { type HttpResponse, noContent, ok } from '@/application/helpers';
 import type { LoadUserExercisesChartsRepository } from '@/domain/contracts/repositories';
 
 type Request = {
   userId: string;
 };
 
+type Model = LoadUserExercisesChartsRepository.Output | null;
+
 export class ListUserExercisesChartsController extends Controller {
   constructor(private readonly exercisesChartsRepository: LoadUserExercisesChartsRepository) {
     super();
   }
 
-  async perform(request: Request): Promise<HttpResponse<null>> {
-    await this.exercisesChartsRepository.loadExercisesCharts(request);
-    return noContent();
+  async perform(request: Request): Promise<HttpResponse<Model>> {
+    const result = await this.exercisesChartsRepository.loadExercisesCharts(request);
+    return result.length ? ok(result) : noContent();
   }
 }
