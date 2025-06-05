0112364ed707dc90fef4ecff6f1b232236aae220
Allysson Alas <allyssonmgs@gmail.com>
2025-06-05 18:54:43 -0300

    feat: add GET /user/:userId/exercisesChart route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -5,11 +5,12 @@ import { Permissions, Roles } from '@/main/enums';
 import {
   makeAuthenticationController,
   makeCreateExercisesChartController,
+  makeListUserExercisesChartsController,
 } from '@/main/factories/application/controllers';
 import { makeAuthorizationMiddleware } from '@/main/factories/application/middlewares';
 import { makeForbidRoleCreationDecorator } from '@/main/factories/main/decorators';
 
-const { CREATE_EXERCISES_CHART } = Permissions;
+const { CREATE_EXERCISES_CHART, LIST_USER_EXERCISES_CHARTS } = Permissions;
 
 export default (router: Router): void => {
   router.post('/user', adaptExpressRoute(makeForbidRoleCreationDecorator([Roles.ADMIN])));
@@ -18,5 +19,10 @@ export default (router: Router): void => {
     '/user/:userId/exercisesChart',
     adaptExpressMiddleware(makeAuthorizationMiddleware(CREATE_EXERCISES_CHART)),
     adaptExpressRoute(makeCreateExercisesChartController()),
+  );
+  router.get(
+    '/user/:userId/exercisesChart',
+    adaptExpressMiddleware(makeAuthorizationMiddleware(LIST_USER_EXERCISES_CHARTS)),
+    adaptExpressRoute(makeListUserExercisesChartsController()),
   );
 };
