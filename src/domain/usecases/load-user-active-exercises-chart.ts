923b96ea1f056504b0b9e6a7588729ee49b015c2
Allysson Alas <allyssonmgs@gmail.com>
2026-02-18 17:48:58 -0300

    feat: ensure LoadUserActiveExercisesChart returns null if activeChartId is undefined

--- src/domain/usecases/load-user-active-exercises-chart.ts
+++ src/domain/usecases/load-user-active-exercises-chart.ts
@@ -1,12 +1,13 @@
 import type { LoadUserByIdRepository } from '@/domain/contracts/repositories';
 
 type Input = { userId: string };
-type Output = void;
+type Output = void | null;
 export type LoadUserActiveExercisesChart = (input: Input) => Promise<Output>;
 type Setup = (userRepository: LoadUserByIdRepository) => LoadUserActiveExercisesChart;
 
 export const setupLoadUserActiveExercisesChart: Setup = (userRepository) => {
   return async ({ userId }) => {
-    await userRepository.loadById({ id: userId });
+    const { activeChartId } = await userRepository.loadById({ id: userId });
+    if (!activeChartId) return null;
   };
 };
