c2420acc74832c389a731299dd9a6c5797823fcb
Allysson Alas <allyssonmgs@gmail.com>
2026-02-19 23:47:10 -0300

    feat: ensure LoadUserActiveExercisesChartController returns 200 on success

--- src/application/controllers/load-user-active-exercises-chart.ts
+++ src/application/controllers/load-user-active-exercises-chart.ts
@@ -1,18 +1,21 @@
 import { Controller } from '@/application/controllers';
-import { type HttpResponse, noContent } from '@/application/helpers';
+import { type HttpResponse, noContent, ok } from '@/application/helpers';
+import type { ExercisesChart } from '@/domain/entities/generic-types';
 import type { LoadUserActiveExercisesChart } from '@/domain/usecases';
 
 type Request = {
   userId: string;
 };
 
+type Model = ExercisesChart | null;
+
 export class LoadUserActiveExercisesChartController extends Controller {
   constructor(private readonly loadUserActiveExercisesChart: LoadUserActiveExercisesChart) {
     super();
   }
 
-  async perform(request: Request): Promise<HttpResponse<null>> {
-    await this.loadUserActiveExercisesChart(request);
-    return noContent();
+  async perform(request: Request): Promise<HttpResponse<Model>> {
+    const result = await this.loadUserActiveExercisesChart(request);
+    return result ? ok(result) : noContent();
   }
 }
