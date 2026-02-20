46a5ff426856b4c0dc1a3407ebfea71407537903
Allysson Alas <allyssonmgs@gmail.com>
2026-02-19 23:35:06 -0300

    test: ensure LoadUserActiveExercisesChartController return 500 if LoadUserActiveExercisesChart throw

--- tests/application/controllers/load-user-active-exercises-chart.spec.ts
+++ tests/application/controllers/load-user-active-exercises-chart.spec.ts
@@ -1,4 +1,5 @@
 import { Controller, LoadUserActiveExercisesChartController } from '@/application/controllers';
+import { ServerError } from '@/application/errors';
 
 describe('LoadUserActiveExercisesChartController', () => {
   const request = {
@@ -25,5 +26,17 @@ describe('LoadUserActiveExercisesChartController', () => {
 
     expect(loadUserActiveExercisesChart).toHaveBeenCalledWith(request);
     expect(loadUserActiveExercisesChart).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should return 500 if LoadUserActiveExercisesChart throws', async () => {
+    const error = new Error('load_user_active_exercises_chart_error');
+    loadUserActiveExercisesChart.mockRejectedValueOnce(error);
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      data: new ServerError(error),
+      statusCode: 500,
+    });
   });
 });
