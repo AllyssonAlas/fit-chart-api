362bb81aa34182c84949add2d4cbce13a2802b5e
Allysson Alas <allyssonmgs@gmail.com>
2026-02-19 23:32:47 -0300

    feat: ensure LoadUserActiveExercisesChartController calls usecase with correct input

--- src/application/controllers/load-user-active-exercises-chart.ts
+++ src/application/controllers/load-user-active-exercises-chart.ts
@@ -1,8 +1,18 @@
 import { Controller } from '@/application/controllers';
 import { type HttpResponse, noContent } from '@/application/helpers';
+import type { LoadUserActiveExercisesChart } from '@/domain/usecases';
+
+type Request = {
+  userId: string;
+};
 
 export class LoadUserActiveExercisesChartController extends Controller {
-  async perform(): Promise<HttpResponse<null>> {
+  constructor(private readonly loadUserActiveExercisesChart: LoadUserActiveExercisesChart) {
+    super();
+  }
+
+  async perform(request: Request): Promise<HttpResponse<null>> {
+    await this.loadUserActiveExercisesChart(request);
     return noContent();
   }
 }
