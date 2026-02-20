19dd0bf74944a06909f80c07c21b92e65bd60886
Allysson Alas <allyssonmgs@gmail.com>
2026-02-19 23:32:58 -0300

    test: ensure LoadUserActiveExercisesChartController calls usecase with correct input

--- tests/application/controllers/load-user-active-exercises-chart.spec.ts
+++ tests/application/controllers/load-user-active-exercises-chart.spec.ts
@@ -1,13 +1,29 @@
 import { Controller, LoadUserActiveExercisesChartController } from '@/application/controllers';
 
 describe('LoadUserActiveExercisesChartController', () => {
+  const request = {
+    userId: 'any_user_id',
+  };
+
   let sut: LoadUserActiveExercisesChartController;
+  let loadUserActiveExercisesChart: jest.Mock;
+
+  beforeAll(() => {
+    loadUserActiveExercisesChart = jest.fn();
+  });
 
   beforeEach(() => {
-    sut = new LoadUserActiveExercisesChartController();
+    sut = new LoadUserActiveExercisesChartController(loadUserActiveExercisesChart);
   });
 
   it('Should extend controller', () => {
     expect(sut).toBeInstanceOf(Controller);
+  });
+
+  it('Should call LoadUserActiveExercisesChart with correct input', async () => {
+    await sut.handle(request);
+
+    expect(loadUserActiveExercisesChart).toHaveBeenCalledWith(request);
+    expect(loadUserActiveExercisesChart).toHaveBeenCalledTimes(1);
   });
 });
